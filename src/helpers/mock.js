4c2dccc1418554cac116b68478f3565d2101ff31
Victoria Fuceneco <fuceneco.vi@gmail.com>
2022-05-26 18:45:01 -0300

    Agrego data de coinGecko y avanzo con la page Coin

--- src/helpers/mock.js
+++ src/helpers/mock.js
@@ -244,3 +244,224 @@ export const coinsData = [
 export const trendingCoins = [coinsData[3], coinsData[0], coinsData[17]];
 export const perfCoins = [coinsData[0], coinsData[8], coinsData[15]];
 export const worstCoins = [coinsData[19], coinsData[6], coinsData[11]];
+
+export const coinGeckoCoin = {
+  id: "bitcoin",
+  symbol: "btc",
+  name: "Bitcoin",
+  hashing_algorithm: "SHA-256",
+  categories: [
+    "Cryptocurrency"
+  ],
+  description: {
+    en: "Bitcoin is the first successful internet money based on peer-to-peer technology; whereby no central bank or authority is involved in the transaction and production of the Bitcoin currency. It was created by an anonymous individual/group under the name, Satoshi Nakamoto. The source code is available publicly as an open source project, anybody can look at it and be part of the developmental process.\r\n\r\nBitcoin is changing the way we see money as we speak. The idea was to produce a means of exchange, independent of any central authority, that could be transferred electronically in a secure, verifiable and immutable way. It is a decentralized peer-to-peer internet currency making mobile payment easy, very low transaction fees, protects your identity, and it works anywhere all the time with no central authority and banks.\r\n\r\nBitcoin is designed to have only 21 million BTC ever created, thus making it a deflationary currency. Bitcoin uses the <a href=\"https://www.coingecko.com/en?hashing_algorithm=SHA-256\">SHA-256</a> hashing algorithm with an average transaction confirmation time of 10 minutes. Miners today are mining Bitcoin using ASIC chip dedicated to only mining Bitcoin, and the hash rate has shot up to peta hashes.\r\n\r\nBeing the first successful online cryptography currency, Bitcoin has inspired other alternative currencies such as <a href=\"https://www.coingecko.com/en/coins/litecoin\">Litecoin</a>, <a href=\"https://www.coingecko.com/en/coins/peercoin\">Peercoin</a>, <a href=\"https://www.coingecko.com/en/coins/primecoin\">Primecoin</a>, and so on.\r\n\r\nThe cryptocurrency then took off with the innovation of the turing-complete smart contract by <a href=\"https://www.coingecko.com/en/coins/ethereum\">Ethereum</a> which led to the development of other amazing projects such as <a href=\"https://www.coingecko.com/en/coins/eos\">EOS</a>, <a href=\"https://www.coingecko.com/en/coins/tron\">Tron</a>, and even crypto-collectibles such as <a href=\"https://www.coingecko.com/buzz/ethereum-still-king-dapps-cryptokitties-need-1-billion-on-eos\">CryptoKitties</a>."
+  },
+  links: {
+    homepage: [
+      "http://www.bitcoin.org",
+      "",
+      ""
+    ],
+    blockchain_site: [
+      "https://blockchair.com/bitcoin/",
+      "https://btc.com/",
+      "https://btc.tokenview.com/",
+      "https://explorer.energi.network/token/0x29a791703e5a5a8d1578f8611b4d3691377cebc0",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ],
+    official_forum_url: [
+      "https://bitcointalk.org/",
+      "",
+      ""
+    ],
+    chat_url: [
+      "",
+      "",
+      ""
+    ],
+    announcement_url: [
+      "",
+      ""
+    ],
+    twitter_screen_name: "bitcoin",
+    facebook_username: "bitcoins",
+    bitcointalk_thread_identifier: null,
+    telegram_channel_identifier: "",
+    subreddit_url: "https://www.reddit.com/r/Bitcoin/",
+    repos_url: {
+      github: [
+        "https://github.com/bitcoin/bitcoin",
+        "https://github.com/bitcoin/bips"
+      ],
+      bitbucket: []
+    }
+  },
+  image: {
+    thumb: "https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579",
+    small: "https://assets.coingecko.com/coins/images/1/small/bitcoin.png?1547033579",
+    large: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+  },
+  country_origin: "",
+  genesis_date: "2009-01-03",
+  sentiment_votes_up_percentage: 60.57,
+  sentiment_votes_down_percentage: 39.43,
+  market_cap_rank: 1,
+  coingecko_rank: 1,
+  coingecko_score: 81.065,
+  developer_score: 98.965,
+  community_score: 74.251,
+  liquidity_score: 99.91,
+  public_interest_score: 0.127,
+  market_data: {
+    current_price: {
+      ars: 3541838,
+      eur: 27671,
+      usd: 29659,
+    },
+    ath: {
+      ars: 6913791,
+      eur: 59717,
+      usd: 69045,
+    },
+    ath_change_percentage: {
+      ars: -48.89761,
+      eur: -53.82013,
+      usd: -57.15072,
+    },
+    ath_date: {
+      ars: "2021-11-10T14:24:11.849Z",
+      eur: "2021-11-10T14:24:11.849Z",
+      usd: "2021-11-10T14:24:11.849Z",
+    },
+    atl: {
+      ars: 1478.98,
+      eur: 51.3,
+      usd: 67.81,
+    },
+    atl_change_percentage: {
+      ars: 238788.90933,
+      eur: 53658.30951,
+      usd: 43530.17906,
+    },
+    atl_date: {
+      ars: "2015-01-14T00:00:00.000Z",
+      eur: "2013-07-05T00:00:00.000Z",
+      usd: "2013-07-06T00:00:00.000Z",
+    },
+    market_cap: {
+      ars: 67472271687771,
+      eur: 527133244834,
+      usd: 564997089804,
+    },
+    market_cap_rank: 1,
+    fully_diluted_valuation: {
+      ars: 74378589346404,
+      eur: 581089478205,
+      usd: 622828985497,
+    },
+    total_volume: {
+      ars: 3804571413804,
+      eur: 29723559388,
+      usd: 31858594990,
+    },
+    high_24h: {
+      ars: 3568989,
+      eur: 28075,
+      usd: 29978,
+    },
+    low_24h: {
+      ars: 3392848,
+      eur: 26511,
+      usd: 28412,
+    },
+    price_change_24h: 151.329,
+    price_change_percentage_24h: 0.51285,
+    price_change_percentage_7d: 3.08048,
+    price_change_percentage_14d: 2.57677,
+    price_change_percentage_30d: -26.74896,
+    price_change_percentage_60d: -33.36787,
+    price_change_percentage_200d: -51.90096,
+    price_change_percentage_1y: -22.38184,
+    market_cap_change_24h: 2941923996,
+    market_cap_change_percentage_24h: 0.52342,
+    price_change_24h_in_currency: {
+      ars: 28512,
+      eur: -5.375077766420873,
+      usd: 151.33,
+    },
+    price_change_percentage_1h_in_currency: {
+      ars: 1.08653,
+      eur: 1.1854,
+      usd: 1.08746,
+    },
+    price_change_percentage_24h_in_currency: {
+      ars: 0.81154,
+      eur: -0.01942,
+      usd: 0.51285,
+    },
+    price_change_percentage_7d_in_currency: {
+      ars: 4.32603,
+      eur: 0.7094,
+      usd: 3.08048,
+    },
+    price_change_percentage_14d_in_currency: {
+      ars: 4.61996,
+      eur: 0.61279,
+      usd: 2.57677,
+    },
+    price_change_percentage_30d_in_currency: {
+      ars: -23.72296,
+      eur: -26.78177,
+      usd: -26.74896
+    },
+    price_change_percentage_60d_in_currency: {
+      ars: -27.894,
+      eur: -31.713,
+      usd: -33.36787,
+    },
+    price_change_percentage_200d_in_currency: {
+      ars: -42.50523,
+      eur: -48.16188,
+      usd: -51.90096,
+    },
+    price_change_percentage_1y_in_currency: {
+      ars: -1.62044,
+      eur: -11.30711,
+      usd: -22.38184,
+    },
+    market_cap_change_24h_in_currency: {
+      ars: 550194832835,
+      eur: -46959990.623168945,
+      usd: 2941923996,
+    },
+    market_cap_change_percentage_24h_in_currency: {
+      ars: 0.82214,
+      eur: -0.00891,
+      usd: 0.52342,
+    },
+    total_supply: 21000000,
+    max_supply: 21000000,
+    circulating_supply: 19050075,
+    last_updated: "2022-05-26T16:31:26.242Z"
+  },
+  community_data: {
+    facebook_likes: null,
+    twitter_followers: 5291383,
+    reddit_average_posts_48h: 6.2,
+    reddit_average_comments_48h: 759.2,
+    reddit_subscribers: 4230599,
+    reddit_accounts_active_48h: 4965,
+    telegram_channel_user_count: null
+  },
+  public_interest_stats: {
+    alexa_rank: 9440,
+    bing_matches: null
+  },
+  status_updates: [],
+  last_updated: "2022-05-26T16:31:26.242Z"
+};
--- src/pages/Coin.js
+++ src/pages/Coin.js
@@ -1,25 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Chip, Container, Typography } from '@mui/material';
+import { Box, Button, Chip, Container, Typography } from '@mui/material';
 import { useParams } from "react-router-dom";
 
 import News from '../sections/News';
 import Loading from '../components/Loading';
+
+import { coinGeckoCoin } from '../helpers/mock';
+import LinkIcon from '@mui/icons-material/Link';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import CodeIcon from '@mui/icons-material/Code';
+import ArticleIcon from '@mui/icons-material/Article';
+
+import MenuButton from '../components/MenuButton';
 
 const Coin = () => {
   const { id } = useParams();
 
   const [coin, setCoin] = useState({});
   const [loading, setLoading] = useState(true);
-  
+
+
   useEffect(() => {
     setTimeout(() => {
       setCoin({
-        id: "bitcoin",
-        symbol: "btc",
-        name: "Bitcoin",
+        id: coinGeckoCoin.id,
+        symbol: coinGeckoCoin.symbol,
+        name: coinGeckoCoin.name,
+        logo: coinGeckoCoin.image.small,
+        rank: coinGeckoCoin.market_cap_rank,
+        description: coinGeckoCoin.description.en,
+        explorer: coinGeckoCoin.blockchain_site
       });
       setLoading(false);
-    }, 80000);
+    }, 0);
   }, []);
 
   return (
@@ -30,10 +43,34 @@ const Coin = () => {
             <Loading size="7rem"/>
           </Box>
         ) : (
-          <Typography variant="h1">
-            {coin.name}
-            <Chip sx={{textTransform: 'uppercase'}} label={coin?.symbol.toUpperCase()} size="small" />
-          </Typography>
+          <Box>
+            <Box sx={{ display: 'flex' }}>
+              <img src={coin.logo} alt={coin.name}/>
+              <Typography component='h1' variant='h2'>{coin.name}</Typography>
+              <Chip sx={{textTransform: 'uppercase'}} label={coin?.symbol.toUpperCase()} size="small" />
+            </Box>
+            <Box>
+              <Chip label={`Rank #${coin.rank}`}></Chip>
+            </Box>
+            <Box>
+              <Button variant="contained" href="http://www.google.com">
+                <LinkIcon />
+                bitcoin.org
+                <OpenInNewIcon />
+              </Button>
+              <MenuButton label={coinGeckoCoin.name} options={coinGeckoCoin.explorer} />
+              <Button variant="contained" href="http://www.google.com">
+                <CodeIcon />
+                Source code
+                <OpenInNewIcon />
+              </Button>
+              <Button variant="contained" href="http://www.google.com">
+                <ArticleIcon />
+                Whitepaper
+                <OpenInNewIcon />
+              </Button>
+            </Box>
+          </Box>
         )
       }
       <News />
