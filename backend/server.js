ac7a80aee9542f1a8f28bfb48bcfe57dba8e6a65
Victoria Fuceneco <fuceneco.vi@gmail.com>
2022-06-22 14:18:13 -0300

    Arreglo problemas de backend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cookieParser = require("cookie-parser");
-const querystring = require('querystring');
 const CryptocurrencyRoutes = require("./routes/cryptocurrency");
 const UserRoutes = require("./routes/users");
 const app = express();
 
 require('dotenv').config();
 
-ATLAS_USER = process.env.ATLAS_USER || '';
-ATLAS_PASS = process.env.ATLAS_PASS || '';
-BACKEND_PORT = process.env.BACKEND_PORT || 3001;
+const ATLAS_USER = process.env.ATLAS_USER || '';
+const ATLAS_PASS = process.env.ATLAS_PASS || '';
+const BACKEND_PORT = process.env.BACKEND_PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
