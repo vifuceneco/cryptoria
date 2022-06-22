91ea2483d69532fca91f60b60fd590549add1ea6
Victoria Fuceneco <fuceneco.vi@gmail.com>
2022-06-22 14:19:25 -0300

    Arreglo problemas de variables en getData

--- backend/getData.js
+++ backend/getData.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const CryptocurrencyModel = require('./models/Cryptocurrency')
 const coinUrl = "https://api.coingecko.com/api/v3/coins/";
 require('dotenv').config();
-ATLAS_USER = process.env.ATLAS_USER || '';
-ATLAS_PASS = process.env.ATLAS_PASS || '';
+const ATLAS_USER = process.env.ATLAS_USER || '';
+const ATLAS_PASS = process.env.ATLAS_PASS || '';
 
 mongoose
   .connect("mongodb+srv://" + ATLAS_USER + ":" + ATLAS_PASS + "@ayi-cluster.jhcut.mongodb.net/?retryWrites=true&w=majority")
@@ -25,4 +25,4 @@ const getCoin = (id = "") => {
 
 // const coins = ["bitcoin", "ethereum", "tether", "usd-coin", "binancecoin", "cardano", "ripple", "binance-usd", "solana", "dogecoin", "polkadot", "wrapped-bitcoin", "tron", "staked-ether", "avalanche-2", "dai", "shiba-inu", "leo-token", "crypto-com-chain", "litecoin", "matic-network", "chainlink", "ftx-token", "near", "stellar", "chain-2", "monero", "bitcoin-cash", "okb", "ethereum-classic", "algorand", "cosmos", "flow", "uniswap", "vechain", "tezos", "theta-fuel", "hedera-hashgraph", "apecoin", "the-sandbox", "axie-infinity", "kucoin-shares", "internet-computer", "filecoin", "frax", "decentraland", "elrond-erd-2", "compound-ether", "theta-token", "aave"];
 
-coins.forEach(coin => getCoin(coin));
+// coins.forEach(coin => getCoin(coin));
